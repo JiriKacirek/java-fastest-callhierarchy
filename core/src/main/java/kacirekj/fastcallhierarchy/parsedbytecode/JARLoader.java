1ef9fd46634c7068326aaec96ea3dedce7d7ca86
Jiri Kacirek <kacirek.j@gmail.com>
2018-06-09 10:35:39 +0200

    All Jars in Jar are now extracted recursively

--- core/src/main/java/kacirekj/fastcallhierarchy/parsedbytecode/JARLoader.java
+++ core/src/main/java/kacirekj/fastcallhierarchy/parsedbytecode/JARLoader.java
@@ -1,46 +1,112 @@
 package kacirekj.fastcallhierarchy.parsedbytecode;
 
 import kacirekj.fastcallhierarchy.parsedbytecode.clazz.Clazz;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 import org.objectweb.asm.ClassReader;
 
 import java.io.*;
-import java.util.Enumeration;
-import java.util.HashSet;
-import java.util.Set;
+import java.net.URLDecoder;
+import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.stream.Collectors;
 
 /**
  * Parse compiled classes in JAR file into Clazz objects.
  */
 public class JARLoader {
-    private Set<Clazz> parsedClasses = new HashSet<>();
-    JarFile jarFile;
 
-    public JARLoader(JarFile jar) throws IOException {
-        jarFile = jar;
-        loadJAR();
+    private File tempDir;
+    private JarFile jarFile;
+    private final String TEMP_DIR = "tmp";
+
+    public JARLoader(JarFile jarFile) throws UnsupportedEncodingException {
+        String currentLocation = URLDecoder.decode(
+                JARLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath(),
+                "UTF-8");
+        this.tempDir = new File(currentLocation + File.separator + TEMP_DIR);
+        this.jarFile = jarFile;
     }
 
-    private void loadJAR() throws IOException {
+    public Set<Clazz> parseArtifact(File artifact) throws IOException, ZipException {
+        // extract recursively all JAR in artifact into tempDir
+        extractAllJarsRecursively(artifact, tempDir);
+
+        // get all previously extracted files
+        List<File> allExtractedFiles = new ArrayList<>();
+        listFilesRecursively(tempDir, allExtractedFiles);
+
         ClassParserVisitor completeClassVisitor = new ClassParserVisitor();
+        Set<Clazz> parsedClasses = new HashSet<>();
 
-        Enumeration<JarEntry> entries = jarFile.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
+        allExtractedFiles.stream().filter(f -> f.getName().endsWith(".class"))
+                .forEach(clazz -> {
+                    try (FileInputStream fis = new FileInputStream(clazz);
+                         InputStream stream = new BufferedInputStream(fis, 1024);)
+                    {
+                        ClassReader reader = new ClassReader(stream);
+                        reader.accept(completeClassVisitor, 0);
+                        parsedClasses.add(completeClassVisitor.getParsedClassIfReady());
 
-            if (entry.getName().endsWith(".class")) {
-                InputStream stream = new BufferedInputStream(jarFile.getInputStream(entry), 1024);
-                ClassReader reader = new ClassReader(stream);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
 
-                reader.accept(completeClassVisitor, 0);
-                parsedClasses.add(completeClassVisitor.getParsedClassIfReady());
+        // delete temporary dir
 
-                stream.close();
+        tempDir.delete();
+
+        return parsedClasses;
+    }
+
+    private void extractAllJarsRecursively(File input, File output) throws IOException, ZipException {
+        if (!output.exists()) {
+            output.mkdir();
+        }
+
+        extractZip(input, output);
+
+        while (true) {
+            List<File> files = new ArrayList<>();
+            listFilesRecursively(output, files);
+            List<File> jars = files.stream().filter(x -> x.getName().endsWith(".jar")).collect(Collectors.toList());
+
+            if (jars.size() == 0) {
+                break;
+            }
+
+            jars.stream().parallel().forEach(jar -> {
+
+                extractZip(jar, output);
+                System.out.println(jar.getName() + " deleted: " + jar.delete());
+            });
+        }
+    }
+
+    private void extractZip(File input, File output) {
+        try {
+            ZipFile zipFile = new ZipFile(input);
+            zipFile.extractAll(output.getPath());
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // get all the files from a directory
+    public void listFilesRecursively(File dir, List<File> files) {
+        File[] fList = dir.listFiles();
+        for (File file : fList) {
+            if (file.isFile()) {
+                files.add(file);
+            } else if (file.isDirectory()) {
+                listFilesRecursively(file, files);
             }
         }
     }
-    public Set<Clazz> getParsedClasses() {
-        return parsedClasses;
-    }
+
+
 }
--- core/src/test/java/kacirekj/fastcallhierarchy/QuickStartGuideIT.java
+++ core/src/test/java/kacirekj/fastcallhierarchy/QuickStartGuideIT.java
@@ -12,29 +12,32 @@ import kacirekj.fastcallhierarchy.parsedbytecode.clazz.Clazz;
 import kacirekj.fastcallhierarchy.parsedbytecode.clazz.Method;
 import kacirekj.fastcallhierarchy.utils.Utils;
 import kacirekj.fastcallhierarchy.datastructures.Node;
+import net.lingala.zip4j.exception.ZipException;
 import org.junit.Before;
 import org.junit.Test;
 
 public class QuickStartGuideIT {
-	JARLoader jarLoader;
 	Method calle = new Method("cz/george/testmodule/A methodForA ()V"); // method call
+	Set<Clazz> parsedClasses;
 
 	@Before
-	public void init() throws IOException {
+	public void init() throws IOException, ZipException {
 		// 0. Load testing JAR file (replace with your JAR)
 		String runPath = URLDecoder.decode(
 				QuickStartGuideIT.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "UTF-8");
-		File testJAR = new File(runPath + File.separator + "fastcallhierarchy-testproject-1.0.1.jar");
+		File testJAR = new File( "C:\\Users\\Jiri Kacirek\\Downloads\\liferay-ce-portal-tomcat-7.0-ga6-20180320170724974.zip");
 
 		// 1. Create JARLoader.
 		JarFile jarFile = new JarFile(testJAR);
-		jarLoader = new JARLoader(jarFile);
+
+		// 2. Parse all classes in the given JAR
+		JARLoader jarLoader = new JARLoader(jarFile);
+		parsedClasses = jarLoader.parseArtifact(testJAR);
+
 	}
 	
 	@Test
 	public void printCallers() {
-		// 2. Parse all classes in the given JAR
-		Set<Clazz> parsedClasses = jarLoader.getParsedClasses();
 
 		// 3. Put and transofrm parsed classes into container (it will create hashmaps above this classes for quicker searching)
 		ParsedClassesContainer parsedClassesContainer = new ParsedClassesContainer(parsedClasses);
